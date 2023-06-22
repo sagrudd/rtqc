0f2a5af87c405357202ffa06709981cc67940083
Stephen Rudd <stephen@mnemosyne.co.uk>
2023-06-22 15:00:00 +0200

    concept of async across packages

---
+++ testing.R
@@ -0,0 +1,16 @@
+# rextendr::document()
+
+
+dorado_fastq_path <- rtqc::get_bundled_path("fastq_pass")
+single_fastq <- file.path(
+  dorado_fastq_path, list.files(dorado_fastq_path)[1])
+
+bf <- basecalled_folder$new(dorado_fastq_path)
+bf$index(threads=1)
+bf$status()
+
+bf$status()
+
+bf$status()
+
+seqset <- bf$as_sequence_set()
